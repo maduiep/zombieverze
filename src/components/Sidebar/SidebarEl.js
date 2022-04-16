18ea5acdcecb0ccb1940424eac6a2709117b683b
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2022-04-16 17:26:54 +0100

    added sidebar

---
+++ src/components/Sidebar/SidebarEl.js
@@ -0,0 +1,108 @@
+import styled from "styled-components"
+import { FaTimes } from 'react-icons/fa'
+import { Link as LinkRouter } from 'react-router-dom'
+import { Link as LinkScroll } from 'react-scroll'
+
+
+export const SidebarContainer = styled.aside `
+    position: fixed;
+    z-index: 1000;
+    display: grid;
+    width: 80%;
+    height: 100%;
+    background: var(--primary-color);
+    top: 0;
+    left: 0;
+    transition: 0.3s ease-in-out;
+    opacity: ${({isOpen}) => (isOpen ? '100%' : '0')};
+    top: ${({ isOpen }) => (isOpen ? '0' : '-100%')};  
+`
+
+
+export const CloseIcon = styled(FaTimes)`
+    color: var(--white-color);
+`
+
+export const Icon = styled.div `
+    position: absolute;
+    top: 1.2rem;
+    right: 1.5rem;
+    background: transparent;
+    font-size: 2rem;
+    cursor: pointer;
+    outline: none;
+`
+export const SidebraWrapper = styled.div `
+    color: var(--white-color);
+
+`
+
+export const SidebarMenu = styled.ul `
+    display: grid;
+    grid-template-columns: 1fr;
+    grid-template-rows: repeat(8, 60px);
+    text-align: center;
+    margin-top: 3rem;
+
+    @media screen and (max-width: 480px) {
+        grid-template-rows: repeat(8, 40px);
+    }
+`
+
+export const SidebarLink = styled(LinkScroll)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 1.5rem;
+    text-decoration: none;
+    list-style: none;
+    transition: 0.2s ease-in-out;
+    text-decoration: none;
+    color: var(--white-color);
+    font-family: 'Days One, 'sans-serif';
+    cursor: pointer;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        color: var(--accent-color);
+    }
+
+    @media screen and (max-width: 680px) {
+        font-size: 1rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        &:hover {
+        transition: all 0.2s ease-in-out;
+        color: var(--accent-color);
+        }
+    }
+`
+
+export const SidebarBtnWrap = styled.div `
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const SidebarRoute = styled(LinkRouter)`
+    border-radius: 50px;
+    background: var(--accent-color);
+    white-space: nowrap;
+    padding: 16px 64px;
+    color: var(--white-color);
+    font-size: 16px;
+    outline: none;
+    border: none;
+    cursor: pointer;
+    transition: all 0.2s ease-in-out;
+    text-decoration: none;
+
+    &:hover {
+        transition: all 0.2s ease-in-out;
+        background: var(--white-color);
+        color: var(--accent-color);
+        outline: none;
+    }
+`
