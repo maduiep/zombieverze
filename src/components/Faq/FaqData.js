3d874b1b6461a38ffd8a11d55a3c0ff85fc6dfd0
Emmanuel Madu IP <emmanuelmaduip@gmail.com>
2022-04-18 23:46:56 +0100

    correct faq and herosection images

--- src/components/Faq/FaqData.js
+++ src/components/Faq/FaqData.js
@@ -1,5 +1,5 @@
 export const FaqData = [{
-        question: 'What is Zombieverse Club?',
+        question: 'What is Zombieverze Club?',
         answer: "ZombieVerze is an open-world rpg gaming nft project that is launching in the metaverse.The game is a blockchain - powered survival MMO that sees players work together as either humans or zombies to scavenge and fight each other for survival and claim resources and territories "
     },
 
@@ -14,7 +14,7 @@ export const FaqData = [{
     },
 
     {
-        question: 'How many Zombieverse NFT’s will there be ?',
+        question: 'How many Zombieverze NFT’s will there be ?',
         answer: '9,999 Participants 8,888 Zombies 1,111 Humans Which colony will you fall into!'
     },
 
@@ -24,19 +24,30 @@ export const FaqData = [{
     },
 
     {
-        question: 'What is the minting price for Zombieverse NFT?',
+        question: 'What is the minting price for Zombieverze NFT?',
         answer: 'Unlimited'
     },
 
 
     {
-        question: 'What is the minting price for Zombieverse NFT?',
-        answer: 'WL price: 0.05 ETH Public Sale Price: 0.075 ETH '
+        question: 'What is the minting price for Zombieverze NFT?',
+        answer: 'WL price: 104 Matic Public Sale Price: 140 Matic.'
     },
 
 
     {
         question: 'Can we mint on desktop or mobile?',
         answer: 'Yes'
+    },
+
+    {
+        question: 'Launch',
+        answer: "The official launch day is designed to give you complete access to all the benefits and packages. With so much ease, you canget onboarded andget seasoned mouth watery returns."
+    },
+
+
+    {
+        question: 'Airdrop Token',
+        answer: "The airdrop Zene token is accessible to all holders of the Zombie NFT. This juicy token confers on you the rights of a vested interest partner."
     }
 ]
